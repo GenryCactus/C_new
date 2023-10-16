075f87f08791aa7ff52b0c52015d09b979e17f24
Genry <amveu_ctrimer@mail.ru>
2023-10-16 23:41:52 +0300

    1 D/z

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Введите число: ");
+string stringNumber = Console.ReadLine();
+int number; 
+bool isNumber = int.TryParse(stringNumber, out number);
+Console.WriteLine($"Длина строки(кол-во символов): {stringNumber.Length}");
+if (isNumber && stringNumber.Length <= 3) // isNumber == True
+{
+    Console.WriteLine("Ваша строчка состоит ТОЛЬКО из цифр");
+    Console.WriteLine($"{stringNumber} => {stringNumber[1]}");
+}
+else
+{
+    Console.WriteLine(" длина строки < 3 символов");
+}
