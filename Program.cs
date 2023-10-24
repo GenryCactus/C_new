0df41d38d538d12ff6768d451866306738e0d8bd
Genry <amveu_ctrimer@mail.ru>
2023-10-24 21:56:28 +0300

    D/z 4 functiom

--- Program.cs
+++ Program.cs
@@ -1,71 +1,75 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
-
-
+﻿
 // Console.Write("Введите число: ");
 // string stringNumber = Console.ReadLine();
 // int number; 
 // bool isNumber = int.TryParse(stringNumber, out number);
-// Console.WriteLine($"Длина строки(кол-во символов): {stringNumber.Length}");
-// if (isNumber && stringNumber.Length <= 3) // isNumber == True
+// if (isNumber && stringNumber.Length >= 6) 
 // {
-//     Console.WriteLine($"{stringNumber} => {stringNumber[1]}");
+//     Console.WriteLine("Число не пятизначное");
+//     if ( stringNumber.Length <= 4) 
+//     {
+//      Console.WriteLine("Число не пятизначное");
+//     }
 // }
+// else if (stringNumber [0] == stringNumber[4] && stringNumber [1] == stringNumber[3])
+// {    
+//      Console.WriteLine($"{stringNumber} -> True");
+// }  
 // else
 // {
-//     Console.WriteLine(" длина строки < 3 символов");
+//  Console.WriteLine($"{stringNumber} -> False");
 // }
 
 
-// второй вариант решения задачи
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
 
-// Console.Write("Введите число: ");
+// Console.Write("Введите число А: ");
 // int number = Convert.ToInt32(Console.ReadLine());
-// int firstDigit = number / 100; 
-// int thirdDigit = number % 10; 
-// int secondDigit = number - firstDigit*100 - thirdDigit;
-// int result = secondDigit/10;
-// Console.WriteLine($"{number} -> {result}");
-
-
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
-
-
-// Console.Write("Введите число: ");
-// string stringNumber = Console.ReadLine();
-// int number; 
-// bool isNumber = int.TryParse(stringNumber, out number);
-// Console.WriteLine($"Длина строки(кол-во символов): {stringNumber.Length}");
-// if (isNumber && stringNumber.Length >= 3) // isNumber == True
-// {
-//     Console.WriteLine($"{stringNumber} => {stringNumber[2]}");
-// }
-// else
-// {
-//     Console.WriteLine("третьей цифры нет");
+// Console.Write("Введите число B: ");
+// int Degreed = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{number} в натуральной степени {Degreed} = {GetDegree(number, Degreed)}");
+// double GetDegree (int number,int Degreed)
+// { 
+//     int degree = Degreed ;
+//     int chapt = number;
+//     double pow = Math.Pow(chapt,degree );
+//     return pow ;
 // }
 
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
 
-// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-// 6 -> да
-// 7 -> да
-// 1 -> нет
-// Console.Write("Введите число N: ");
-// int N= Convert.ToInt32(Console.ReadLine());
-// if (N >7 )
+// Console.Write("Введите число : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Сумма всех чисел {number} -> {SumAllDigit(number)} ");
+// int SumAllDigit(int number)
 // {
-//  Console.WriteLine($"{N} -> число больше дня недели");
+//  int result = 0 ;
+//  while (number> 0)
+//  {
+//   result+= number %10;
+//   number = number/10; 
+//  }
+//  return result;
 // }
-//  else if (N == 7 || N == 6)
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] result = GetBinaryArray(8); 
+//  Console.WriteLine($"[{String.Join("; ", result)}]");
+
+// int[] GetBinaryArray(int size)
 // {
-//  Console.WriteLine($"{N} -> да ");
-// }
-// else 
-// {
-//   Console.WriteLine($"{N} -> нет");
+//     int[] array = new int[size];
+//     for (int i = 0; i < array.Length; i++) // array.Length = size
+//     {
+//         array[i] = new Random().Next(0,100); 
+//     }
+//     return array;
 // }
